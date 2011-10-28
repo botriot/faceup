f908779bd4e8174ef6827652e864bd530044c355
Wade Simmons <wade@wades.im>
2011-10-27 17:36:56 -0700

    don't use cluster for now

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var cluster = require('cluster');
+require('./app').listen(process.env.PORT || 3000)
 
-cluster('./app')
-  .set('workers', 1)
-  .use(cluster.debug())
-  .use(cluster.logger('logs'))
-  .use(cluster.pidfiles('pids'))
-  .listen(process.env.PORT || 3000);
+// var cluster = require('cluster');
+// 
+// cluster('./app')
+//   .set('workers', 1)
+//   .use(cluster.debug())
+//   .listen(process.env.PORT || 3000);
