91cfa0388a221f8329ba48f1c149dfa829dac248
Wade Simmons <wade@wades.im>
2012-04-05 21:27:36 -0700

    node 0.6

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
 require('./app').listen(process.env.PORT || 3000)
-
-// var cluster = require('cluster');
-// 
-// cluster('./app')
-//   .set('workers', 1)
-//   .use(cluster.debug())
-//   .listen(process.env.PORT || 3000);
