6b62fdb8ac9a08d25b533f0c1ebddd74e58a482b
Jon Rohan <rohan@github.com>
2012-08-24 16:30:47 -0700

    if an image is stashable. @wadey

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/stashable', routes.stashable);
 app.get('/img', routes.img);
 app.get('/img.jpg', routes.img);
 app.get('/img.jpeg', routes.img);
--- routes/index.js
+++ routes/index.js
@@ -22,3 +22,22 @@ exports.img = function(req, res, next) {
     res.send(image, {'Content-Type': 'image/jpeg'}, 200)
   })
 };
+
+exports.stashable = function(req, res, next) {
+  var data = {
+    image: req.param('src', 'http://www.librarising.com/astrology/celebs/images2/QR/queenelizabethii.jpg'),
+    overlay: req.param('overlay', 'mustache'),
+    method: req.param('method', 'cv')
+  }
+
+  image.mash(data, function(err, image) {
+    if (err) {
+      console.log(err.stack || err)
+      return res.send({"stashable":"false"}, {}, 200)
+    }
+    if (!image) {
+      return res.send({"stashable":"false"}, {}, 200)
+    }
+    return res.send({"stashable":"true"}, {}, 200)
+  })
+};
